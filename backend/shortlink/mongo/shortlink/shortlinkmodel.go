aa5ce4ecc02cfa5fe353547916dcddf9996729b5
libing <laoxiao1886@gmail.com>
2025-03-11 11:23:36 +0800

    feat(shortlink):删除逻辑

--- backend/shortlink/mongo/shortlink/shortlinkmodel.go
+++ backend/shortlink/mongo/shortlink/shortlinkmodel.go
@@ -74,6 +74,7 @@ func (c *customShortlinkModel) UpdateShortLinkInfo(ctx context.Context, data *Sh
 
 	filter := bson.M{
 		"origin_url": data.OriginUrl,
+		"gid":        data.Gid,
 		"deleteFlag": constant.ENABLE_FLAG,
 	}
 
