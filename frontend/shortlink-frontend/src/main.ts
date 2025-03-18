dfc6324a3c0b1616d28983c089c5049b481cb94e
libing <laoxiao1886@gmail.com>
2025-03-18 21:32:03 +0800

    feat:frontend 引入ant designer组建库

--- frontend/shortlink-frontend/src/main.ts
+++ frontend/shortlink-frontend/src/main.ts
@@ -3,9 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.use(Antd).use(router).mount('#app')
